15ed3c0e1b0ae2fa1bbc6cf6c74fbfd9b2e9562d
Zheng Qu <quzhengrobot@gmail.com>
2020-02-13 23:38:09 +0100

    ADD: first structure with triple buffer

---
+++ examples/triple_buffer_example.cpp
@@ -0,0 +1,26 @@
+#include <triple_buffer.h>
+#include <chrono>
+#include <iostream>
+
+using namespace std::chrono_literals;
+
+int main(int argc, char* argv[]) {
+  TripleBuffer<double> buf;
+  std::atomic<bool> stop{false};
+
+  std::thread print_thread([&buf, &stop] {
+    while (!stop) {
+      std::cout << buf << std::endl;
+    }
+  });
+
+  for (size_t i = 0; i < 10; i++) {
+    buf = i;
+    std::this_thread::sleep_for(200ms);
+  }
+
+  std::this_thread::sleep_for(1s);
+  stop = true;
+  print_thread.join();
+  return 0;
+}
